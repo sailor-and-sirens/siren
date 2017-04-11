6e597f2ddaf0cf5f4c412bd3de2453c4776f4401
lmegviar <lmegviar@gmail.com>
2017-04-11 17:00:08 -0400

    Add icons to header

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { actionCreators } from '../actions';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const mapStateToProps = (state) => ({
   view: state.view
@@ -14,11 +15,11 @@ class Header extends Component {
       <View style={styles.container}>
         <Text style={styles.header}>{this.props.view}</Text>
         <View style={styles.navigation}>
-          <Text style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Inbox'))}>I</Text>
-          <Text style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Playlist'))}>P</Text>
-          <Text style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Search'))}>S</Text>
-          <Text style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Time Filter'))}>T</Text>
-          <Text style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Settings'))}>S</Text>
+          <MaterialIcons style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Inbox'))} size={30} name='inbox'/>
+          <MaterialIcons style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Playlist'))} size={30} name='playlist-play'/>
+          <MaterialIcons style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Search'))} size={30} name='search'/>
+          <MaterialIcons style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Filter'))} size={30} name='filter-list' />
+          <MaterialIcons style={styles.navigationLink} onPress={() => this.props.dispatch(actionCreators.changeView('Settings'))} size={30} name='settings' />
         </View>
       </View>
     );
