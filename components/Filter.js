cd439fd44a9896412e10005b9ef145f54bcf3e11
lmegviar <lmegviar@gmail.com>
2017-04-13 11:33:37 -0400

    Scroll view for picker in IOS

--- components/Filter.js
+++ components/Filter.js
@@ -34,54 +34,54 @@ class Filter extends Component {
   render() {
    return (
       <View style={styles.mainView}>
-
-        <View style={styles.filterBar}>
-          <Ionicons style={styles.icon} size={30} color='grey' name="ios-heart-outline"/>
-          <Picker itemStyle={styles.pickerItem} style={styles.picker}
-              selectedValue={this.state.liked}
-              onValueChange={(value) => {this.setState({liked: value});}}>
-            <Picker.Item style={styles.pickerItem} value="likedOff" label="All" />
-            <Picker.Item style={styles.pickerItem} value="liked" label="Liked" />
-            <Picker.Item style={styles.pickerItem} value="notLiked" label="Not Liked" />
-          </Picker>
-        </View>
-
-        <View style={styles.filterBar}>
-            <Ionicons style={styles.bookmark} size={30} color='grey' name="ios-bookmark-outline"/>
-            <Picker style={styles.picker}
-                selectedValue={this.state.bookmarked}
-                onValueChange={(value) => {this.setState({bookmarked: value});}}>
-              <Picker.Item value="bookmarkedOff" label="All" />
-              <Picker.Item value="bookmarked" label="Bookmarked" />
-              <Picker.Item value="notBookmarked" label="Not Bookmarked" />
+        <ScrollView>
+          <View style={styles.filterBar}>
+            <Ionicons style={styles.icon} size={30} color='grey' name="ios-heart-outline"/>
+            <Picker itemStyle={styles.pickerItem} style={styles.picker}
+                selectedValue={this.state.liked}
+                onValueChange={(value) => {this.setState({liked: value});}}>
+              <Picker.Item style={styles.pickerItem} value="likedOff" label="All" />
+              <Picker.Item style={styles.pickerItem} value="liked" label="Liked" />
+              <Picker.Item style={styles.pickerItem} value="notLiked" label="Not Liked" />
             </Picker>
-        </View>
+          </View>
 
-        <View style={styles.filterBar}>
-          <Image source={require('../assets/clockIcons/clock5.png')} style={styles.clock} />
+          <View style={styles.filterBar}>
+              <Ionicons style={styles.bookmark} size={30} color='grey' name="ios-bookmark-outline"/>
+              <Picker style={styles.picker}
+                  selectedValue={this.state.bookmarked}
+                  onValueChange={(value) => {this.setState({bookmarked: value});}}>
+                <Picker.Item value="bookmarkedOff" label="All" />
+                <Picker.Item value="bookmarked" label="Bookmarked" />
+                <Picker.Item value="notBookmarked" label="Not Bookmarked" />
+              </Picker>
+          </View>
+
+          <View style={styles.filterBar}>
+            <Image source={require('../assets/clockIcons/clock5.png')} style={styles.clock} />
+            <Picker style={styles.picker}
+                selectedValue={this.state.time}
+                onValueChange={(value) => {this.setState({time: value});}}>
+              <Picker.Item value="timeOff" label="All" />
+              <Picker.Item value="5" label="< 5 min" />
+              <Picker.Item value="15" label="< 15 min" />
+              <Picker.Item value="30" label="< 30 min" />
+              <Picker.Item value="45" label="< 45 min" />
+              <Picker.Item value="60" label="< 60 min" />
+              <Picker.Item value="60+" label="> 60 min" />
+            </Picker>
+          </View>
+        </ScrollView>
+          <View style={styles.filterBar}>
+          <Ionicons style={styles.icon} size={30} color='grey' name="ios-pricetag-outline"/>
           <Picker style={styles.picker}
-              selectedValue={this.state.time}
-              onValueChange={(value) => {this.setState({time: value});}}>
-            <Picker.Item value="timeOff" label="All" />
-            <Picker.Item value="5" label="< 5 min" />
-            <Picker.Item value="15" label="< 15 min" />
-            <Picker.Item value="30" label="< 30 min" />
-            <Picker.Item value="45" label="< 45 min" />
-            <Picker.Item value="60" label="< 60 min" />
-            <Picker.Item value="60+" label="> 60 min" />
+                selectedValue={this.state.tag}
+                onValueChange={(value, i) => {this.setState({tag: value});}}>
+              {this.getTags().map((tag, i) => (
+                <Picker.Item value={tag} label={tag} key={i}/>
+              ))}
           </Picker>
-        </View>
-
-        <View style={styles.filterBar}>
-        <Ionicons style={styles.icon} size={30} color='grey' name="ios-pricetag-outline"/>
-        <Picker style={styles.picker}
-              selectedValue={this.state.tag}
-              onValueChange={(value, i) => {this.setState({tag: value});}}>
-            {this.getTags().map((tag, i) => (
-              <Picker.Item value={tag} label={tag} key={i}/>
-            ))}
-        </Picker>
-        </View>
+          </View>
         <View style={styles.buttonView}>
           <Button style={styles.done} color='grey' onPress={() =>{this.props.dispatch(actionCreators.toggleModal()); this.props.dispatch(actionCreators.updateInboxFilters(this.state));}} title='Filter'/>
         </View>
