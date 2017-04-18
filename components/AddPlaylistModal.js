2a04cc609b7c5ddf42f891adafd1b804e25c3f10
Emilie <emiliehester@gmail.com>
2017-04-18 16:01:34 -0600

    Add layout and styling for Add to Playlist Modal

--- components/AddPlaylistModal.js
+++ components/AddPlaylistModal.js
@@ -1,5 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View, Modal, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Modal, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const playlists = [
+  {id: 1, name: 'Monday', totalEpisodes: 4, totalTime: '180+', isSelected: false},
+  {id: 2, name: 'Tuesday', totalEpisodes: 1, totalTime: '75', isSelected: false},
+  {id: 3, name: 'Wednesday', totalEpisodes: 2, totalTime: '90', isSelected: false},
+  {id: 4, name: 'Thursday', totalEpisodes: 3, totalTime: '120', isSelected: true},
+  {id: 5, name: 'Friday', totalEpisodes: 0, totalTime: '0', isSelected: false},
+  {id: 6, name: 'Weekend', totalEpisodes: 1, totalTime: '56', isSelected: false},
+  {id: 7, name: 'Productivity Mix', totalEpisodes: 5, totalTime: '180+', isSelected: false},
+  {id: 8, name: 'A Really Long Title', totalEpisodes: 2, totalTime: '65', isSelected: false},
+  {id: 9, name: 'Health & Wellness', totalEpisodes: 3, totalTime: '130', isSelected: false},
+  {id: 10, name: 'Soul Food', totalEpisodes: 4, totalTime: '180+', isSelected: false},
+  {id: 11, name: 'WebDev', totalEpisodes: 2, totalTime: '85', isSelected: false},
+  {id: 12, name: 'Entrepreneurship', totalEpisodes: 3, totalTime: '120', isSelected: false}
+]
+
+const playlistStyle = (index) => {
+  if (index % 2 === 0) {
+    return [styles.playlistWrapper, styles.playlistAltBackground];
+  }
+  return styles.playlistWrapper;
+}
+
+const iconStyle = (isSelected) => {
+  if (isSelected) {
+    return (
+      <MaterialIcons size={25} name='playlist-add-check' color='#2EAC6D'></MaterialIcons>
+    )
+  }
+  return (
+    <MaterialIcons size={25} name='playlist-add'></MaterialIcons>
+  )
+}
+
+const cancelSaveButton = (props) => {
+  if (playlists.some(playlist => playlist.isSelected)) {
+    return (
+      <View style={styles.cancelSaveWrapper}>
+        <TouchableOpacity onPress={props.handleAddToPlaylistModalClose} style={styles.cancelButton}>
+          <Text style={styles.cancelSaveText}>Cancel</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={props.handleAddToPlaylistModalClose} style={styles.saveButton}>
+          <Text style={styles.cancelSaveText}>Save</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+  return (
+    <View style={styles.cancelSaveWrapper}>
+      <TouchableOpacity onPress={props.handleAddToPlaylistModalClose} style={styles.cancelButton}>
+        <Text style={styles.cancelSaveText}>Cancel</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 const AddPlaylistModal = (props) => {
   return (
@@ -10,8 +66,36 @@ const AddPlaylistModal = (props) => {
       onRequestClose={() => console.log('point me to a function')}
     >
       <View style={styles.container}>
-        <Text>Add Playlist Modal</Text>
-        <TouchableOpacity onPress={props.handleAddToPlaylistModalClose}><Text>x close me</Text></TouchableOpacity>
+        <ScrollView style={styles.scrollWrapper}>
+          <View style={styles.topWrapper}>
+            <Text style={styles.topHeading}>Select Playlist to Add Episode</Text>
+            <View style={styles.createPlaylistWrapper}>
+              <TextInput style={styles.addPlaylistInput} placeholder="Enter Name of New Playlist"/>
+              <View style={styles.addPlaylistButton}>
+                <Text style={styles.addPlaylistButtonText}>Create + Add</Text>
+              </View>
+            </View>
+          </View>
+          {playlists.map((playlist, index) => (
+            <View style={playlistStyle(index)} key={index}>
+              <View style={styles.playlistIconWrapper}>
+                {iconStyle(playlist.isSelected)}
+              </View>
+              <View style={styles.playlistNameWrapper}>
+                <Text style={styles.playlistName}>{playlist.name}</Text>
+              </View>
+              <View style={styles.playlistTotalEpisodesWrapper}>
+                <Text style={styles.totalHeading}>Total Episodes</Text>
+                <Text style={styles.totalNumber}>{playlist.totalEpisodes}</Text>
+              </View>
+              <View style={styles.playlistTotalDurationWrapper}>
+                <Text style={styles.totalHeading}>Total Time</Text>
+                <Text style={styles.totalNumber}>{playlist.totalTime}min</Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+        {cancelSaveButton(props)}
       </View>
     </Modal>
   )
@@ -20,9 +104,129 @@ const AddPlaylistModal = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 30,
+    backgroundColor: '#ffffff'
+  },
+  scrollWrapper: {
+    marginBottom: 50
+  },
+  topWrapper: {
+    marginLeft: 5,
+    marginRight: 5,
+    paddingTop: 10,
+    backgroundColor: '#eeeeee',
+    borderWidth: 1,
+    borderColor: '#bbbbbb'
+  },
+  topHeading: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  createPlaylistWrapper: {
+    height: 70,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    padding: 5,
+  },
+  addPlaylistInput: {
+    height: 40,
+    flex: 0.65,
+    alignSelf: 'center',
+    marginRight: 5,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#bbbbbb',
+    backgroundColor: '#ffffff'
+  },
+  addPlaylistButton: {
+    height: 40,
+    flex: 0.35,
+    justifyContent: 'center',
+    backgroundColor: '#2EAC6D'
+  },
+  addPlaylistButtonText: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontSize: 12,
+  },
+  playlistWrapper: {
+    height: 60,
+    flexDirection: 'row',
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    paddingTop: 8,
+    paddingBottom: 8,
+    backgroundColor: '#eeeeee',
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderColor: '#bbbbbb'
+  },
+  playlistAltBackground: {
+    backgroundColor: '#ffffff'
+  },
+  playlistIconWrapper: {
+    height: '100%',
+    flex: 0.09,
+    justifyContent: 'center',
+    paddingRight: 10,
+  },
+  playlistNameWrapper: {
+    height: '100%',
+    flex: 0.51,
+    justifyContent: 'center',
+    paddingRight: 5,
+  },
+  playlistName: {
+    fontSize: 16
+  },
+  playlistTotalEpisodesWrapper: {
+    height: '100%',
+    flex: 0.325,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  playlistTotalDurationWrapper: {
+    height: '100%',
+    flex: 0.325,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  totalHeading: {
+    fontSize: 12,
+    textAlign: 'center'
+  },
+  totalNumber: {
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  cancelSaveWrapper: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    height: 50,
+    width: '100%',
+    flexDirection: 'row'
+  },
+  cancelSaveText: {
+    color: '#ffffff'
+  },
+  cancelButton: {
+    height: '100%',
+    flex: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#b00404'
+  },
+  saveButton: {
+    height: '100%',
+    flex: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2EAC6D'
   }
 })
 
 export default AddPlaylistModal;
+
+// visible={props.isAddPlaylistModalVisible}
